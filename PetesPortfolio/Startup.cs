03a4f0637a1224d147e943ffbcc3e47904d52a89
Greentruk <pete@peteharvey.com>
2016-07-12 14:09:42 -0400

    first upload

---
+++ PetesPortfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetesPortfolio.Startup))]
+namespace PetesPortfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
